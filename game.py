4bc81ed4251c28b392fd6a887cea761e98e0a9bf
Christine Parfitt <christineparfitt@MacBook-Pro.local>
2022-09-25 15:51:05 +0800

    Monster Game

---
+++ game.py
@@ -0,0 +1,8 @@
+player = {'name': 'Christine', 'attack': 10, 'heal': 16, 'health': 100}
+monster = {'name': 'Max', 'attack': 12, 'health': 100}
+
+print('Please select action')
+print('1) Attack')
+print('2) Heal')
+input()
+
