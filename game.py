491ae42896f47776dad9f08c636e1d898697f01e
Christine Parfitt <christineparfitt@MacBook-Pro.local>
2022-09-26 15:29:05 +0800

    game exists when I die

--- game.py
+++ game.py
@@ -1,8 +1,26 @@
 player = {'name': 'Christine', 'attack': 10, 'heal': 16, 'health': 100}
 monster = {'name': 'Max', 'attack': 12, 'health': 100}
+game_running = True
 
-print('Please select action')
-print('1) Attack')
-print('2) Heal')
-input()
+while game_running == True :
 
+    print('Please select action')
+    print('1) Attack')
+    print('2) Heal')
+
+    player_choice = input()
+
+    if player_choice == '1' : 
+        monster['health'] = monster['health'] - player['attack']
+        player['health'] = player['health'] - monster['attack']
+        print(monster['health'])
+        print(player['health'])
+
+    elif player_choice == '2' :
+        print('Health')
+    else :
+        print('Error')
+    
+    if player['health'] <= 0 or monster['health'] <= 0 :
+        game_running = False
+        print('Either you or the Monster Died')
