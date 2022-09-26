e077ae1eb632bf8c9db43ad225219205a4215636
Christine Parfitt <christineparfitt@MacBook-Pro.local>
2022-09-26 16:02:05 +0800

    we've created a game that plays many rounds and resets all the health attributes at the beginning of the game, we've also added an exit game function.

--- game.py
+++ game.py
@@ -1,26 +1,45 @@
-player = {'name': 'Christine', 'attack': 10, 'heal': 16, 'health': 100}
-monster = {'name': 'Max', 'attack': 12, 'health': 100}
+
 game_running = True
 
 while game_running == True :
+    new_round = True
+    player = {'name': 'Christine', 'attack': 19, 'heal': 16, 'health': 100}
+    monster = {'name': 'Max', 'attack': 12, 'health': 100}  
+    while new_round == True :
+        player_won = False
+        monster_won = False
 
-    print('Please select action')
-    print('1) Attack')
-    print('2) Heal')
+        print('Please select action')
+        print('1) Attack')
+        print('2) Heal')
+        print('3) Exit game')
 
-    player_choice = input()
+        player_choice = input()
 
-    if player_choice == '1' : 
-        monster['health'] = monster['health'] - player['attack']
-        player['health'] = player['health'] - monster['attack']
-        print(monster['health'])
-        print(player['health'])
+        if player_choice == '1' : 
+            monster['health'] = monster['health'] - player['attack']
+            if monster['health'] <= 0 :
+                    player_won = True
+            else :
+                player['health'] = player['health'] - monster['attack']
+                if player['health'] <= 0 :
+                    monster_won = True
 
-    elif player_choice == '2' :
-        print('Health')
-    else :
-        print('Error')
-    
-    if player['health'] <= 0 or monster['health'] <= 0 :
-        game_running = False
-        print('Either you or the Monster Died')
+
+            print(monster['health'])
+            print(player['health'])
+
+        elif player_choice == '2' :
+            print('Health')
+        elif player_choice == '3' :
+            game_running = False
+            new_round = False
+
+        else :
+            print('Error')
+        
+        if player_won == True or monster_won == True :
+            #player['health'] = 100
+            #monster['health'] = 100
+            new_round = False
+            print('New round!')
